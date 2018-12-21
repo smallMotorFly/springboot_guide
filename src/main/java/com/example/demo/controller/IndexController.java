3e833b29c1d0a9d4421b692cbade31756317615d
zhangbin <bin.zhang@juneyaokc.com>
2018-12-21 15:29:12 +0800

    SpringBoot + jsp 测试

--- src/main/java/com/example/demo/controller/IndexController.java
+++ src/main/java/com/example/demo/controller/IndexController.java
@@ -1,17 +1,16 @@
 package com.example.demo.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by zhangbin on 2018/12/21.
  */
-//@RestController
-//@RequestMapping
-public class HelloControler {
-//    @RequestMapping(value = "/index",method = RequestMethod.GET)
+@Controller
+public class IndexController {
+    @RequestMapping(value = "index",method = RequestMethod.GET)
     public String index() {
-        return "Hello,world!";
+        return "index";
     }
 }
